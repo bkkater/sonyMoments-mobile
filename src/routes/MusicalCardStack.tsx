952c3049c2cc9a4d1f6e3a896c06a6a0e0eef462
bkkater <biancamedeiros.bs10@gmail.com>
2020-11-01 15:12:11 -0300

    [add] MusicalStack stack

---
+++ src/routes/MusicalCardStack.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+import MusicalCard01 from '../pages/MusicalCard/MusicalCard01';
+import MusicalCard02 from '../pages/MusicalCard/MusicalCard02';
+import MusicalCard03 from '../pages/MusicalCard/MusicalCard03';
+import MusicalCard04 from '../pages/MusicalCard/MusicalCard04';
+import MusicalCard05 from '../pages/MusicalCard/MusicalCard05';
+import MusicalCard06 from '../pages/MusicalCard/MusicalCard06';
+import MusicalCard07 from '../pages/MusicalCard/MusicalCard07';
+import Playlist from '../pages/Playlist';
+
+
+
+const { Screen, Navigator } = createStackNavigator();
+
+function MusicalCardStack() {
+    return (
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen name='MusicalCard01' component={MusicalCard01} />
+            <Screen name='MusicalCard02' component={MusicalCard02} />
+            <Screen name='MusicalCard03' component={MusicalCard03} />
+            <Screen name='MusicalCard04' component={MusicalCard04} />
+            <Screen name='MusicalCard05' component={MusicalCard05} />
+            <Screen name='MusicalCard06' component={MusicalCard06} />
+            <Screen name='MusicalCard07' component={MusicalCard07} />
+
+            <Screen name='Playlist' component={Playlist} />
+
+
+
+        </Navigator>
+    )
+}
+
+export default MusicalCardStack;
