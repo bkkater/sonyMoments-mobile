5caa9a2e872cbdb34225ad3a397d58d841ddbe88
bkkater <biancamedeiros.bs10@gmail.com>
2020-11-01 15:11:56 -0300

    [update] routes

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -3,6 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
 import Landing from '../pages/Landing';
 import Login from '../pages/Login';
+import Register01 from '../pages/Register/Register01';
+import Register02 from '../pages/Register/Register02';
+
+import MenuTabs from './MenuTabs';
 
 const { Screen, Navigator } = createStackNavigator();
 
@@ -12,6 +16,10 @@ function AppStack() {
             <Navigator screenOptions={{headerShown: false}}> 
                 <Screen name='Landing' component={Landing} />
                 <Screen name='Login' component={Login} />
+
+                <Screen name='MenuTabs' component={MenuTabs} />
+                <Screen name='Register01' component={Register01} />
+                <Screen name='Register02' component={Register02} />
 
             </Navigator>
         </NavigationContainer>
---
+++ src/routes/MenuTabs.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+
+import Home from '../pages/Home';
+import MusicalCard01 from '../pages/MusicalCard/MusicalCard01';
+import MusicalCardStack from './MusicalCardStack';
+
+const Tab = createBottomTabNavigator();
+
+export default function MenuTabs() {
+
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                style: {
+                    elevation: 0,
+                    shadowOpacity: 0,
+                    height: 75,
+                    position: 'absolute',
+                    borderTopColor: 'black'
+                },
+                tabStyle: {
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                },
+                labelStyle: {
+                    fontFamily: 'Ubuntu_700Bold',
+                    fontSize: 11,
+                    marginBottom: 10
+                },
+                keyboardHidesTabBar: true,
+                inactiveBackgroundColor: '#261125',
+                activeBackgroundColor: '#261125',
+                inactiveTintColor: 'transparent',
+                activeTintColor: 'transparent',
+
+            }}>
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarLabel: 'Início',
+                    tabBarIcon: ({ color, size, focused }) => {
+                        return (
+                            <Feather name="home" size={30} color={focused ? '#901193' : '#87838E'} style={{marginTop: 15}}/>
+                        );
+                    }
+                }} />
+            <Tab.Screen
+                name="Cam"
+                component={MusicalCardStack}
+                options={{
+                    tabBarLabel: 'Cam',
+                    tabBarIcon: ({ color, size, focused }) => {
+                        return (
+                                <Feather name="camera" size={35} color={focused ? '#901193' : '#87838E'} style={{marginTop: 15}}/>
+                        );
+                    }
+                }} />
+            <Tab.Screen
+                name="Ajustes"
+                component={Home}
+                options={{
+                    tabBarLabel: 'Ajustes',
+                    tabBarIcon: ({ color, size, focused }) => {
+                        return (
+                            <Feather name="users" size={30} color={focused ? '#901193' : '#87838E'} style={{marginTop: 15}}/>
+                        );
+                    }
+                }} />
+
+        </Tab.Navigator>
+    );
+}
